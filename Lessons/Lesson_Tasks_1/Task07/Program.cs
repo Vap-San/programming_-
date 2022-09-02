f9b1d7b507807c0b29aa9573404cd2b765311aa8
Владимир  Петров <petrov@vgapo.int>
2022-09-02 15:59:55 +0300

    Добавлена 7 задача в практикум обратное число

---
+++ Lessons/Lesson_Tasks_1/Task07/Program.cs
@@ -0,0 +1,32 @@
+﻿//. Напишите программу вычисления модуля числа.
+// 2-> 2
+// - 3-> 3
+// - 7-> 7
+
+
+double Promt(string message)
+{
+    System.Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0"; //?? "0" - если ничего не ввел, то ставим 0. Вводим числа как строки т.к. это консоль
+    double value = double.Parse(strValue); //Преобразование строки в целое число 
+    return value;
+}
+
+
+double Reverse(double inNumber)
+{
+    return 1/inNumber;    
+}
+
+double Number = Promt("Введите целое число N -> ");
+
+if (Number != 0)
+{
+System.Console.WriteLine(Reverse(Number));    
+}
+else
+{
+System.Console.WriteLine("Ошибка! На ноль делить нельзя");    
+}
+
+
