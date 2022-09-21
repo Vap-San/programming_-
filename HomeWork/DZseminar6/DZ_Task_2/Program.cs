4808f1e9f400d929af9cb4ff94bde1cb775edb88
Vladimir <vapsan@gmail.com>
2022-09-21 08:34:49 +0300

    Задача 2 через функции (практика N 6)

--- HomeWork/DZseminar6/DZ_Task_2/Program.cs
+++ HomeWork/DZseminar6/DZ_Task_2/Program.cs
@@ -13,19 +13,30 @@ int Prompt(string message)
     }
     throw new Exception("Вы ввели не число");
 }
+int InputRatio(string messageK, string messageB, out int b)
+{
+    int k = Prompt(messageK);
+    b = Prompt(messageB);
+    return k;
+}
+void FindIntersectionPoint(int k1, int b1, int k2, int b2)
+{
+    if (k1 != k2)
+    {
+        double x = (double)(b2 - b1) / (k1 - k2);
+        double y = (double)(k1 * b2 - k2 * b1) / (k1 - k2);
+        System.Console.WriteLine($"Прямые пересекаются в точке ({x};{y})");
+    }
+    else
+    {
+        System.Console.WriteLine($"Прямые параллельны, точки пересечения нет");
+    }
 
+}
 //основная программа
-int k1 = Prompt("Введите коэффициент k1 -> ");
-int b1 = Prompt("Введите коэффициент b1 -> ");
-int k2 = Prompt("Введите коэффициент k2 -> ");
-int b2 = Prompt("Введите коэффициент b2 -> ");
-if (k1 != k2)
-{
-    double x = (double)(b2 - b1) / (k1 - k2);
-    double y = (double)(k1 * b2 - k2 * b1) / (k1 - k2);
-    System.Console.WriteLine($"Прямые пересекаются в точке ({x};{y})");
-}
-else
-{
-    System.Console.WriteLine($"Прямые параллельны, точки пересечения нет");
-}
+int b1, b2;
+int k1 = InputRatio("Введите коэффициент k1 ->", "Введите коэффициент b1 ->", out b1);
+int k2 = InputRatio("Введите коэффициент k2 ->", "Введите коэффициент b2 ->", out b2);
+FindIntersectionPoint(k1, b1, k2, b2);
+
+
